2266548e179b248ccefa855b0fd6d6d913f95a11
胡东 <hudong.tech@gmail.com>
2021-10-11 04:35:35 +0800

    基础服务相关配置

--- gulimall-product/src/main/java/com/tech/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/tech/gulimall/product/GulimallProductApplication.java
@@ -1,9 +1,11 @@
 package com.tech.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tech.gulimall.product.dao")
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/test/java/com/tech/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/tech/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,28 @@
 package com.tech.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.tech.gulimall.product.entity.BrandEntity;
+import com.tech.gulimall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class GulimallProductApplicationTests {
+@RunWith(SpringRunner.class)
+@Slf4j
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("戴尔");
+        brandService.save(brandEntity);
+        System.out.println("保存成功！");
     }
 
 }
