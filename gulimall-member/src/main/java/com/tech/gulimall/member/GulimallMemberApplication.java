8543e2bb1c56b0a24b913f37558d1f90280898ed
胡东 <hudong.tech@gmail.com>
2021-10-13 00:45:37 +0800

    更新数据库为Everest Centos 包扫描配置@MapperScan

--- gulimall-member/src/main/java/com/tech/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/tech/gulimall/member/GulimallMemberApplication.java
@@ -1,9 +1,11 @@
 package com.tech.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tech.gulimall.member.dao")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
--- gulimall-order/src/main/java/com/tech/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/tech/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,11 @@
 package com.tech.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tech.gulimall.order.dao")
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
