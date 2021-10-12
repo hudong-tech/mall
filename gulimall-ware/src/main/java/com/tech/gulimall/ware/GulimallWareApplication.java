1a110625aa968b3e6b7fe8004ca23f7f455901a9
胡东 <hudong.tech@gmail.com>
2021-10-13 00:45:37 +0800

    更新数据库为Everest Centos 包扫描配置@MapperScan

--- gulimall-ware/src/main/java/com/tech/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/tech/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,11 @@
 package com.tech.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tech.gulimall.ware.dao")
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
