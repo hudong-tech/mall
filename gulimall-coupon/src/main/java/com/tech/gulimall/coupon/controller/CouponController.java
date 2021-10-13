429336e60ab5a1b6ac17919085d28a0701260881
胡东 <hudong.tech@gmail.com>
2021-10-13 21:31:45 +0800

    nacos 配置中心 测试

--- gulimall-coupon/src/main/java/com/tech/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/tech/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import com.tech.gulimall.common.utils.R;
 import com.tech.gulimall.coupon.entity.CouponEntity;
 import com.tech.gulimall.coupon.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -19,11 +21,24 @@ import java.util.Map;
  * @email hudong.tech@gmail.com
  * @date 2021-10-12 20:49:42
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name", name).put("age", age);
+    }
 
 
     @PostMapping("/member/list")
