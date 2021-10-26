dae6ece7537f546fda819fd691e4ddcc24baf2d6
胡东 <hudong.tech@gmail.com>
2021-10-27 02:20:03 +0800

    获取OSS服务端签名

---
+++ gulimall-third-party/src/main/java/com/tech/gulimall/thirdparty/service/OssService.java
@@ -0,0 +1,13 @@
+package com.tech.gulimall.thirdparty.service;
+
+import java.util.Map;
+
+/**
+* @Description:  对象存储oss service接口
+* @Author: phil
+* @Date: 2021/10/27 1:40
+*/
+public interface OssService {
+
+    Map<String, String> policy();
+}
