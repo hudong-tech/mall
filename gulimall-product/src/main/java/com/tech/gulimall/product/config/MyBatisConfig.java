cb4053ee4282ae53fdb88a387025b1f61726904b
phil <hudong.tech@gmail.com>
2021-11-21 20:47:10 +0800

    乐观锁插件

--- gulimall-product/src/main/java/com/tech/gulimall/product/config/MyBatisConfig.java
+++ gulimall-product/src/main/java/com/tech/gulimall/product/config/MyBatisConfig.java
@@ -1,5 +1,6 @@
 package com.tech.gulimall.product.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.tech.gulimall.product.dao")
 public class MyBatisConfig {
 
-    //引入分页插件
+    /**
+     * 引入分页插件
+     * @return
+     */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
@@ -20,5 +24,17 @@ public class MyBatisConfig {
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
         paginationInterceptor.setLimit(1000);
         return paginationInterceptor;
+    }
+
+    /***
+     * @Description: 添加乐观锁插件配置
+     * @Param: []
+     * @return: com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor
+     * @Author: phil
+     * @Date: 2021/11/21 18:28
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
     }
 }
