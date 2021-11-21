8050dd0c20c7ff1a8f28829eeff9e00d21af109e
phil <hudong.tech@gmail.com>
2021-11-21 20:50:58 +0800

    除商品服务外的mybatis plus配置类实现 1.分页工具配置 2.乐观锁配置

---
+++ gulimall-order/src/main/java/com/tech/gulimall/order/config/MyBatisConfig.java
@@ -0,0 +1,40 @@
+package com.tech.gulimall.order.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.tech.gulimall.order.dao")
+public class MyBatisConfig {
+
+    /**
+     * 引入分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        paginationInterceptor.setOverflow(true);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInterceptor.setLimit(1000);
+        return paginationInterceptor;
+    }
+
+    /***
+     * @Description: 添加乐观锁插件配置
+     * @Param: []
+     * @return: com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor
+     * @Author: phil
+     * @Date: 2021/11/21 18:28
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
