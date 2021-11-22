a7b9716286e5df8dcbb77f59a13af9988b263315
phil <hudong.tech@gmail.com>
2021-11-22 13:49:56 +0800

    封装集合工具类

---
+++ gulimall-common/src/main/java/com/tech/gulimall/common/utils/CollectionUtils.java
@@ -0,0 +1,11 @@
+package com.tech.gulimall.common.utils;
+
+/**
+ * @className: CollectionUtils
+ * @description: 集合工具类
+ * @author: niaonao
+ * @date: 2021/11/22
+ **/
+public class CollectionUtils extends org.apache.commons.collections.CollectionUtils {
+
+}
