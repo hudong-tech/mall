7c19eafb7126d420d8a93fa868dcdfea405b66cf
phil <hudong.tech@gmail.com>
2022-04-15 05:23:10 +0800

    仓储服务-采购需求-增加检索条件

--- gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,16 +1,17 @@
 package com.tech.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tech.gulimall.common.utils.PageUtils;
 import com.tech.gulimall.common.utils.Query;
-
+import com.tech.gulimall.common.utils.StringUtils;
 import com.tech.gulimall.ware.dao.PurchaseDetailDao;
 import com.tech.gulimall.ware.entity.PurchaseDetailEntity;
 import com.tech.gulimall.ware.service.PurchaseDetailService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("purchaseDetailService")
@@ -18,9 +19,35 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        /**
+         * key: 2
+         * status: 0
+         * wareId: 1
+         */
+        LambdaQueryWrapper<PurchaseDetailEntity> queryWrapper = new LambdaQueryWrapper<>();
+
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotEmpty(wareId)) {
+            queryWrapper.eq(PurchaseDetailEntity::getWareId, wareId);
+        }
+
+        String status = (String) params.get("status");
+        if (StringUtils.isNotEmpty(status)) {
+            queryWrapper.eq(PurchaseDetailEntity::getStatus, status);
+        }
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            queryWrapper.and(wrapper -> {
+                wrapper.eq(PurchaseDetailEntity::getPurchaseId, key).or()
+                        .eq(PurchaseDetailEntity::getSkuId, key);
+            });
+        }
+
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
