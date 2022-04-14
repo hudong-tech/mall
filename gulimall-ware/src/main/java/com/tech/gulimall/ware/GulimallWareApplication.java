ba01d31df7506bce2f0a9e03ac2785239eb4ff8d
phil <hudong.tech@gmail.com>
2022-04-15 04:17:12 +0800

    仓储服务-仓库列表-模拟查询

--- gulimall-ware/src/main/java/com/tech/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/tech/gulimall/ware/GulimallWareApplication.java
@@ -4,9 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+// 开启事务
+@EnableTransactionManagement
 @MapperScan("com.tech.gulimall.ware.dao")
 public class GulimallWareApplication {
 
--- gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.tech.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tech.gulimall.common.utils.PageUtils;
 import com.tech.gulimall.common.utils.Query;
-
+import com.tech.gulimall.common.utils.StringUtils;
 import com.tech.gulimall.ware.dao.WareInfoDao;
 import com.tech.gulimall.ware.entity.WareInfoEntity;
 import com.tech.gulimall.ware.service.WareInfoService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,20 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        LambdaQueryWrapper<WareInfoEntity> queryWrapper = new LambdaQueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            // 仓库编号、仓库名字、仓库地址、区域编号
+            queryWrapper.eq(WareInfoEntity::getId, key).or()
+                    .like(WareInfoEntity::getName, key).or()
+                    .like(WareInfoEntity::getAddress, key).or()
+                    .like(WareInfoEntity::getAreacode, key);
+        }
+        // 执行
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
