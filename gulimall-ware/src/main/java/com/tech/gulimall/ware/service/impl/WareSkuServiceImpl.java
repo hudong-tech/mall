f301940ddfe6c479b588947342a597def9fa1f30
phil <hudong.tech@gmail.com>
2022-04-15 04:55:03 +0800

    仓储服务-查询商品库存-增加检索条件

--- gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/tech/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.tech.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tech.gulimall.common.utils.PageUtils;
 import com.tech.gulimall.common.utils.Query;
-
+import com.tech.gulimall.common.utils.StringUtils;
 import com.tech.gulimall.ware.dao.WareSkuDao;
 import com.tech.gulimall.ware.entity.WareSkuEntity;
 import com.tech.gulimall.ware.service.WareSkuService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,25 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        /**
+         * skuId: 3
+         * wareId: 1
+         */
+        LambdaQueryWrapper<WareSkuEntity> queryWrapper = new LambdaQueryWrapper<>();
+
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotEmpty(wareId)) {
+            queryWrapper.eq(WareSkuEntity::getWareId, wareId);
+        }
+
+        String skuId = (String) params.get("skuId");
+        if (StringUtils.isNotEmpty(skuId)) {
+            queryWrapper.eq(WareSkuEntity::getSkuId, skuId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
