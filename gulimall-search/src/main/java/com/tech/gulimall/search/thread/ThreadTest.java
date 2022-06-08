bcd44d555f14d946d74f369689ddb842f23e12f8
phil <hudong.tech@gmail.com>
2022-06-08 22:45:19 +0800

    商城业务-异步-CompletableFuture-handle最终处理

--- gulimall-search/src/main/java/com/tech/gulimall/search/thread/ThreadTest.java
+++ gulimall-search/src/main/java/com/tech/gulimall/search/thread/ThreadTest.java
@@ -19,7 +19,8 @@ public class ThreadTest {
 
 //        runAsync();
 //        supplyAsync();
-        supplyAsyncTest();
+//        supplyAsyncTest();
+        handle();
 
         System.out.println("main方法结束。。。。。");
     }
@@ -43,8 +44,15 @@ public class ThreadTest {
         return future;
     }
 
+    /**
+     * CompletableFuture 带返回值
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static CompletableFuture<Long> supplyAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<Long> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("run start...");
             System.out.println("当前线程： " + Thread.currentThread().getId());
 
             try {
@@ -61,6 +69,38 @@ public class ThreadTest {
         return future;
     }
 
+    /**
+     * CompletableFuture handle最终处理
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public static void handle() throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("当前线程： " + Thread.currentThread().getId());
+
+            int i = 10 / 0;
+
+            System.out.println("运行结果: " + i);
+            return i;
+        }, executor).handle((result, throwable) -> {
+            if (null != result) {
+                return result * 2;
+            }
+            if (null != throwable){
+                System.out.println(throwable.getMessage());
+                return 0;
+            }
+            return -1;
+        });
+        System.out.println(future.get());
+    }
+
+    /**
+     * CompletableFuture 带返回值， 完成回调与异常感知
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static CompletableFuture<Integer> supplyAsyncTest() throws ExecutionException, InterruptedException {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("当前线程： " + Thread.currentThread().getId());
